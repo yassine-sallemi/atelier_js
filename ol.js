84bf22194f1a304aed6083fc1b2ea8401fd9821a
Yassine Sallemi <yessine.sallemi.9@gmail.com>
2024-02-15 19:25:42 +0100

    update: 2nd method added

--- ol.js
+++ ol.js
@@ -5,3 +5,9 @@ li.forEach((el) => {
     e.target.style.color = `#${randomColor}`;
   });
 });
+
+// document.addEventListener("click", (e) => {
+//   if (e.target.tagName !== "LI") return;
+//   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+//   e.target.style.color = `#${randomColor}`;
+// });
