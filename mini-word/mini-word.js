52f961835621dc896b82481f04f0b92e4186c7ba
Yassine Sallemi <yessine.sallemi.9@gmail.com>
2024-02-16 14:14:43 +0100

    update: structure-folders

--- mini-word/mini-word.js
+++ mini-word/mini-word.js
@@ -5,13 +5,12 @@ const fontInput = document.getElementById("font-input");
 
 title.style.fontSize = "32px";
 
-colorInput.addEventListener("change", (e) => {
+colorInput.addEventListener("input", (e) => {
   title.style.color = e.target.value;
 });
 
 numberInput.addEventListener("input", (e) => {
   title.style.fontSize = `${e.target.value}px`;
-  console.log(e.target);
 });
 
 fontInput.addEventListener("input", (e) => {
